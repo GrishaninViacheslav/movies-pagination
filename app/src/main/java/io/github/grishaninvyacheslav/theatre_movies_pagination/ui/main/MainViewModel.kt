c6146bdcd1890402fc655a1d1387abcb5556e42f
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2022-04-09 12:59:30 +0300

    Создал Android-проект

---
+++ app/src/main/java/io/github/grishaninvyacheslav/theatre_movies_pagination/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.grishaninvyacheslav.theatre_movies_pagination.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
